42fe8ae1d5ca3ffbdbc402400fee6237fcad71ba
andruzea <andruzea21@gmail.com>
2026-09-15 23:07:51 -0400

    Add files via upload

---
+++ musicas/playlist.js
@@ -0,0 +1,7 @@
+﻿window.PLAYLIST = [
+  "01 - Bienvenida al Aeropuerto.wav",
+  "02 - Ambiental Sala de Espera.wav",
+  "03 - Jazz Suave Terminal.wav",
+  "04 - Instrumental Vuelos.wav",
+  "05 - Piano Ambiental Embarque.wav"
+];
